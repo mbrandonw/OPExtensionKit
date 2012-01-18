dd42da467446f6a32f28af740b973a289b15f493
Brandon Williams <brandon@kickstarter.com>
2012-01-18 11:59:19 -0500

    Added a header for including everything.

---
+++ OPExtensionKit/Source/OPExtensionKit.h
@@ -0,0 +1,39 @@
+//
+//  OPExtensionKit.h
+//  OPExtensionKit
+//
+//  Created by Brandon Williams on 1/18/12.
+//  Copyright (c) 2012 Opetopic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "NSManagedObjectContext+Opetopic.h"
+#import "NSPersistentStoreCoordinator+Opetopic.h"
+#import "NSArray+Opetopic.h"
+#import "NSCache+Opetopic.h"
+#import "NSData+Opetopic.h"
+#import "NSDate+Opetopic.h"
+#import "NSDateFormatter+Opetopic.h"
+#import "NSDictionary+Opetopic.h"
+#import "NSLocale+Opetopic.h"
+#import "NSMutableArray+Opetopic.h"
+#import "NSMutableDictionary+Opetopic.h"
+#import "NSNotificationCenter+Opetopic.h"
+#import "NSObject+Opetopic.h"
+#import "NSPredicate+Opetopic.h"
+#import "NSSet+Opetopic.h"
+#import "NSString+Opetopic.h"
+#import "NSURL+Opetopic.h"
+#import "NSNumber+Opetopic.h"
+#import "OPMacros.h"
+#import "MKMapView+Opetopic.h"
+#import "UIApplication+Opetopic.h"
+#import "UIDevice+Opetopic.h"
+#import "UIImage+Opetopic.h"
+#import "UINavigationController+Opetopic.h"
+#import "UIProgressView+Opetopic.h"
+#import "UIView+Opetopic.h"
+#import "UIViewController+Opetopic.h"
+#import "UIWebView+Opetopic.h"
+#import "UIColor+Opetopic.h"

ba277042ffe4d29566c484bf0031e1da5738f103
Brandon Williams <brandon@kickstarter.com>
2012-01-26 16:15:14 -0500

    Fixed headers.

--- OPExtensionKit/Source/Core Animation/CAShapeLayer+Opetopic.h
+++ OPExtensionKit/Source/Core Animation/CAShapeLayer+Opetopic.h
@@ -1,9 +1,9 @@
 //
 //  CAShapeLayer+Opetopic.h
-//  Kickstarter
+//  OPExtensionKit
 //
 //  Created by Brandon Williams on 1/26/12.
-//  Copyright (c) 2012 Kickstarter. All rights reserved.
+//  Copyright (c) 2012 Opetopic. All rights reserved.
 //
 
 #import <QuartzCore/QuartzCore.h>

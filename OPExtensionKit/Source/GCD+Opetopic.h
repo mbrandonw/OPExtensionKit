3350a456f804d6ba7b3e5384d19bfa17c8ef67ec
Brandon Williams <brandon@kickstarter.com>
2012-02-16 09:29:04 -0500

    Added some docs.

--- OPExtensionKit/Source/GCD+Opetopic.h
+++ OPExtensionKit/Source/GCD+Opetopic.h
@@ -8,5 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Calls the passed block immediately if we are already executing on the main queue, 
+ otherwise it dispatches to the main queue.
+ */
 void dispatch_asap_main_queue(dispatch_block_t block);
+
+/**
+ Calls the passed block immediately if we are already executing on the supplied queue,
+ otherwise it dispatches it to the queue.
+ */
 void dispatch_asap_queue(dispatch_queue_t queue, dispatch_block_t block);

ef23b9d2f737842145e25faad5803fb1913f41c2
Brandon Williams <brandon@kickstarter.com>
2012-08-16 10:18:00 -0400

    Added category to UIFont for getting all installed fonts.

---
+++ OPExtensionKit/Source/UIKit/UIFont+Opetopic.h
@@ -0,0 +1,15 @@
+//
+//  UIFont+Opetopic.h
+//  Kickstarter
+//
+//  Created by Brandon Williams on 8/15/12.
+//  Copyright (c) 2012 Kickstarter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIFont (Opetopic)
+
++(NSDictionary*) installedFontsByFamily;
+
+@end
---
+++ OPExtensionKit/Source/UIKit/UIFont+Opetopic.m
@@ -0,0 +1,23 @@
+//
+//  UIFont+Opetopic.m
+//  Kickstarter
+//
+//  Created by Brandon Williams on 8/15/12.
+//  Copyright (c) 2012 Kickstarter. All rights reserved.
+//
+
+#import "UIFont+Opetopic.h"
+
+@implementation UIFont (Opetopic)
+
++(NSDictionary*) installedFontsByFamily {
+    
+    NSMutableDictionary *retVal = [NSMutableDictionary new];
+    
+    for (NSString *familyName in [UIFont familyNames])
+        [retVal setObject:[UIFont fontNamesForFamilyName:familyName] forKey:familyName];
+    
+    return retVal;
+}
+
+@end

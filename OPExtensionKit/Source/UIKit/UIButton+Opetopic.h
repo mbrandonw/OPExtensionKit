052e04794ae32eba2480c90a4c1511fe9092bca4
Brandon Williams <brandon@kickstarter.com>
2013-06-08 19:04:51 -0400

    Added a helper on UIButton to center both the icon and title.

---
+++ OPExtensionKit/Source/UIKit/UIButton+Opetopic.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+Opetopic.m
+//  OPExtensionKit
+//
+//  Created by Brandon Williams on 6/8/13.
+//  Copyright (c) 2013 Opetopic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (Opetopic)
+
+-(void) centerTitleAndImageWithSpacing:(CGFloat)spacing;
+
+@end
---
+++ OPExtensionKit/Source/UIKit/UIButton+Opetopic.m
@@ -0,0 +1,26 @@
+//
+//  UIButton+Opetopic.m
+//  OPExtensionKit
+//
+//  Created by Brandon Williams on 6/8/13.
+//  Copyright (c) 2013 Opetopic. All rights reserved.
+//
+
+#import "UIButton+Opetopic.h"
+#import "UIView+Opetopic.h"
+
+@implementation UIButton (Opetopic)
+
+-(void) centerTitleAndImageWithSpacing:(CGFloat)spacing {
+
+  if (self.imageView.image) {
+    CGSize imageSize = self.imageView.size;
+    CGSize titleSize = self.titleLabel.size;
+
+    self.titleEdgeInsets = UIEdgeInsetsMake(0.0, -imageSize.width, -(imageSize.height + spacing), 0.0);
+    titleSize = self.titleLabel.size;
+    self.imageEdgeInsets = UIEdgeInsetsMake(-(titleSize.height + spacing), 0.0, 0.0, - titleSize.width);
+  }
+}
+
+@end
